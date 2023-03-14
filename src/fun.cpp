e1100100de6f35538d6023e0c39f47ffe58697c7
Dobrynia100 <dobrynya2001@gmail.com>
2023-03-14 22:07:42 +0300

    math

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,14 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
 #include <cstring>
-#include "fun.h"
+#include <cmath>
 
+#include "fun.h"
 
 unsigned int faStr1(const char* str) {//нет цифр
     int  N= 0;
     bool let = false, num = false;
-    for (int i = 0; i < strlen(str); i++) 
-    {
+    for (int i = 0; i < strlen(str); i++){
         if (isalpha(str[i])) {
             let = true;
         }
@@ -21,36 +21,24 @@ unsigned int faStr1(const char* str) {//нет цифр
             }
             let = false;
             num = false;
-        }
-         
-        
+        }        
     }
-    
     return N;
 }
-
 unsigned int faStr2(const char* str) {//заглав лат
     int N = 0;
-   
     bool first = true;  
-   
     for (int i = 0; i < strlen(str); i++) {
-      
         if (isupper(str[i])&& first) {
-           
             first = false;
             N++;
-            
         }
-        
         if (isspace(str[i])) {
             first = true;
         }
     }
-  
     return N;
 }
-
 unsigned int faStr3(const char* str) {//сред длин
     int Nwords = 0,Nlet;
     bool word = false;
@@ -63,7 +51,6 @@ unsigned int faStr3(const char* str) {//сред длин
             Nwords++;
             word = false;
         }
-      
     }
     float lenght = static_cast<double>(Nlet) / Nwords;
     return round(lenght);
