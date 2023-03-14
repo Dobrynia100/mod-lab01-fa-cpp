e146659e7f213881d4c41e0d37817f4dc1f86c0f
Dobrynia100 <dobrynya2001@gmail.com>
2023-03-14 21:50:44 +0300

    functions

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,70 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <cstring>
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+
+unsigned int faStr1(const char* str) {//нет цифр
+    int  N= 0;
+    bool let = false, num = false;
+    for (int i = 0; i < strlen(str); i++) 
+    {
+        if (isalpha(str[i])) {
+            let = true;
+        }
+        if (isdigit(str[i])) {
+            num = true;
+        }
+        if (isspace(str[i]&let)) {
+            if (let && !num) {
+                N++;
+            }
+            let = false;
+            num = false;
+        }
+         
+        
+    }
+    
+    return N;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {//заглав лат
+    int N = 0;
+   
+    bool first = true;  
+   
+    for (int i = 0; i < strlen(str); i++) {
+      
+        if (isupper(str[i])&& first) {
+           
+            first = false;
+            N++;
+            
+        }
+        
+        if (isspace(str[i])) {
+            first = true;
+        }
+    }
+  
+    return N;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {//сред длин
+    int Nwords = 0,Nlet;
+    bool word = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            Nlet++;
+            word = true;
+        }
+        if (isspace(str[i])&& word) {
+            Nwords++;
+            word = false;
+        }
+      
+    }
+    float lenght = static_cast<double>(Nlet) / Nwords;
+    return round(lenght);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    const char *str = "qwe4  gftt e 555gr rrgr grhh Lat Nat";
+
+   cout<<faStr1(str)<<endl;
+   cout<<faStr2(str) << endl;
+   cout<<faStr3(str) << endl;
     return 0;
 }
