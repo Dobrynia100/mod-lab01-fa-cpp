4ad70ea21ee07c9cd0dcbff4a872cb10e2bcdfa8
Dobrynia100 <dobrynya2001@gmail.com>
2023-03-14 23:03:17 +0300

    fix

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,52 +6,61 @@
 #include "fun.h"
 
 unsigned int faStr1(const char* str) {//нет цифр
-    int  N= 0;
+    int  N = 0;
     bool let = false, num = false;
-    for (int i = 0; i < strlen(str); i++){
+    for (int i = 0; i < strlen(str); i++) {
         if (isalpha(str[i])) {
             let = true;
         }
         if (isdigit(str[i])) {
             num = true;
         }
-        if (isspace(str[i]&let)) {
+        if (isspace(str[i]) && let) {
             if (let && !num) {
                 N++;
             }
             let = false;
             num = false;
-        }        
+        }
+    }
+    if (let && !num) {
+        N++;
     }
     return N;
 }
 unsigned int faStr2(const char* str) {//заглав лат
     int N = 0;
-    bool first = true;  
+    bool first = true, check = false;
     for (int i = 0; i < strlen(str); i++) {
-        if (isupper(str[i])&& first) {
+        if (isupper(str[i]) && first) {
             first = false;
-            N++;
+
         }
+        if (!isalpha(str[i]) && !isspace(str[i]))check = true;
         if (isspace(str[i])) {
+            if (!check && !first)N++;
             first = true;
+            check = false;
         }
+
     }
+    if (!check && !first)N++;
     return N;
 }
 unsigned int faStr3(const char* str) {//сред длин
-    int Nwords = 0,Nlet;
+    int Nwords = 0, Nlet = 0;
     bool word = false;
     for (int i = 0; i < strlen(str); i++) {
         if (isalpha(str[i])) {
             Nlet++;
             word = true;
         }
-        if (isspace(str[i])&& word) {
+        if (isspace(str[i]) && word) {
             Nwords++;
             word = false;
         }
     }
+    if (word)Nwords++;
     float lenght = static_cast<double>(Nlet) / Nwords;
     return round(lenght);
 }
